50a98ab349ffade17070a7b393d974e5910b25db
askmhs <muhammadharits@icloud.com>
2017-09-12 08:03:03 +0700

    - Create new user command class

---
+++ src/Domain/User/Command/CreateNewUserCommand.js
@@ -0,0 +1,12 @@
+import {User} from "../Model/User";
+import {InvalidException} from "../Exception/InvalidException";
+
+export class CreateNewUserCommand {
+    constructor(userData) {
+        if (userData instanceof User) {
+            this.userData = userData;
+        } else {
+            throw new InvalidException('userData must be implemented from User class!');
+        }
+    }
+}
