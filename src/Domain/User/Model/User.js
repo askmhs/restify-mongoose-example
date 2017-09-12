709a9f84c596369072610fbe4bb3b1f4be3ade6d
askmhs <muhammadharits@icloud.com>
2017-09-12 08:05:00 +0700

    - User model class

---
+++ src/Domain/User/Model/User.js
@@ -0,0 +1,8 @@
+export class User {
+    constructor(_name, _birthDate, _address, _phone) {
+        this.name = _name;
+        this.birthDate = _birthDate;
+        this.address = _address;
+        this.phone = _phone;
+    }
+}
