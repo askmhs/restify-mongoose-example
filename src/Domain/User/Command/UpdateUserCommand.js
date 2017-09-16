f3994ca8949287d2e80335b97aa76e06d1f4e2a5
askmhs <muhammadharits@icloud.com>
2017-09-16 11:33:45 +0700

    - Update user command class

--- src/Domain/User/Command/UpdateUserCommand.js
+++ src/Domain/User/Command/UpdateUserCommand.js
@@ -3,11 +3,10 @@ import {InvalidException} from "../Exception/InvalidException";
 
 export class UpdateUserCommand {
     constructor(userId, data) {
-        if (data instanceof User) {
-            this.userId = userId;
-            this.data = data;
-        } else {
-            throw new InvalidException('data must be implemented from User class!');
-        }
+        if (typeof userId !== 'string') throw new InvalidException('userId must be a string!');
+        if (!(data instanceof User)) throw new InvalidException('data must be instanceof User class!');
+
+        this.userId = userId;
+        this.data = data;
     }
 }
